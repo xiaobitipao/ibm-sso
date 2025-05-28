f833b088ac8c6858754dad829d44d03436a55000
lzzheng <lzzheng@cn.ibm.com>
2025-05-28 20:56:00 +0800

    update: example

--- examples/src/main.py
+++ examples/src/main.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from authlib.integrations.base_client.errors import OAuthError
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
@@ -10,6 +13,8 @@ from starlette import status
 from api.v1 import example_router
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 # Create FastAPI
 app = FastAPI(
@@ -48,11 +53,30 @@ app.add_middleware(
 @app.middleware("http")
 async def add_middleware_process_time_header(request: Request, call_next):
 
-    if request.url.path not in ["/oauth2/login", "/oauth2/token"]:
-        user_info: UserInfoMiddleWareVO = await get_current_user_for_middleware(request)
-        print(f"Login user is: {user_info.email_address}")
+    if request.url.path in ["/oauth2/login", "/oauth2/token"]:
+        return await call_next(request)
 
-    response = await call_next(request)
+    # User info
+    user_info: UserInfoMiddleWareVO = await get_current_user_for_middleware(request)
+    logger.warning(f"Login user is: {user_info.email_address}")
+
+    # Time measurement
+    start_time = time.perf_counter()
+    logger.warning("[API-TRACE] Start Request: %s %s", request.method, request.url.path)
+
+    try:
+        response = await call_next(request)
+    finally:
+        end_time = time.perf_counter()
+        process_time = end_time - start_time
+        logger.warning(
+            "[API-TRACE] End Request:   %s %s - Duration: %.3f seconds",
+            request.method,
+            request.url.path,
+            process_time,
+        )
+        response.headers["X-Process-Time"] = str(round(process_time, 3))
+
     return response
 
 
